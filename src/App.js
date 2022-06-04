10aa7be14b4a768c3370ef90a0ea1f9b54aa069d
crono628 <84817352+crono628@users.noreply.github.com>
2022-06-04 16:04:44 -0400

    weather is destructured and accessible in DailyForecast

--- src/App.js
+++ src/App.js
@@ -6,54 +6,38 @@ import { db } from './firebase';
 import { CircularProgress } from '@mui/material';
 
 const App = () => {
-  const [geocode, setGeocode] = useState(null);
-  const [weather, setWeather] = useState([]);
+  const [weather, setWeather] = useState();
   const [search, setSearch] = useState('');
   const [loading, setLoading] = useState(null);
 
-  useEffect(() => {}, [weather]);
-
-  const getGeocodeInfo = async (input) => {
+  const getWeather = async (input) => {
+    setLoading(true);
     const GEOCODE_ZIPCODE = `http://api.openweathermap.org/geo/1.0/zip?zip=${input}&appid=53dcc962829731a4fa033950e8997254`;
+
     try {
-      let apiCall = await (await fetch(GEOCODE_ZIPCODE)).json();
-      setGeocode(apiCall);
+      const geoCall = await (await fetch(GEOCODE_ZIPCODE)).json();
+      const { lat, lon } = geoCall;
+      const WEATHER_API = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=minutely,alerts&appid=53dcc962829731a4fa033950e8997254`;
+      let weatherCall = await (await fetch(WEATHER_API)).json();
+      setWeather([
+        {
+          currentWeather: weatherCall.current,
+          dailyWeather: [...weatherCall.daily],
+          hourlyWeather: [...weatherCall.hourly],
+          timezoneOffset: weatherCall.timezone_offset,
+        },
+      ]);
     } catch (error) {
       console.log(error);
-    } finally {
-      getComprehensiveWeather();
-      console.log(weather);
-    }
-  };
-
-  const getComprehensiveWeather = async () => {
-    setLoading(true);
-    if (geocode) {
-      const { lat, lon } = geocode;
-      const WEATHER_API = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=minutely,alerts&appid=53dcc962829731a4fa033950e8997254`;
-      try {
-        let apiCall = await (await fetch(WEATHER_API)).json();
-        // console.log(apiCall);
-        setWeather([
-          {
-            currentWeather: [apiCall.current],
-            dailyWeather: [...apiCall.daily],
-            hourlyWeather: [...apiCall.hourly],
-            timezoneOffset: apiCall.timezone_offset,
-          },
-        ]);
-      } catch (error) {
-        console.log(error);
-      }
     }
     setTimeout(() => {
       setLoading(false);
-    }, 2000);
+    }, 1500);
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    getGeocodeInfo(search);
+    getWeather(search);
   };
 
   return (
--- src/components/DailyForecast.js
+++ src/components/DailyForecast.js
@@ -3,11 +3,16 @@ import { Card, CardContent, Paper } from '@mui/material';
 import { Box, Container } from '@mui/system';
 
 const DailyForecast = ({ weather }) => {
+  // let currentWeather = weather.map((item) => item.currentWeather);
+  const [forecast] = weather;
+  const { currentWeather } = forecast;
+
   return (
     <Paper elevation={3} sx={{ backgroundColor: '#91cbf9' }}>
       <Box sx={{ padding: 4 }}>
         <Card>
-          <CardContent>text</CardContent>
+          <CardContent>{Math.round(currentWeather.temp)}</CardContent>
+          {/* {console.log(forecast)} */}
         </Card>
       </Box>
     </Paper>
